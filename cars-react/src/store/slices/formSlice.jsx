7e6fbadaaa88478cc38843b99611f2468d89a33b
L C <LF9904@gmail.com>
2025-07-18 22:01:59 -0400

    added an extraReducers for clearing search input

--- cars-react/src/store/slices/formSlice.jsx
+++ cars-react/src/store/slices/formSlice.jsx
@@ -1,4 +1,5 @@
 import {createSlice} from "@reduxjs/toolkit";
+import { addCar } from "./carsSlice";
 
 const fromSlice = createSlice({
   name: "form",
@@ -14,6 +15,12 @@ const fromSlice = createSlice({
       state.cost = action.payload;
     },
   },
+  extraReducers(builder){
+    builder.addCase(addCar, (state,action) => {
+      state.name = '';
+      state.cost = 0;
+    });
+  }
 });
 
 export const {changeName, changeCost} = fromSlice.actions;
