e0d6b9e80301580974b59a76db843f34082d8962
L C <LF9904@gmail.com>
2025-07-18 21:22:10 -0400

    added rendered list of cars

--- cars-react/src/components/CarList.jsx
+++ cars-react/src/components/CarList.jsx
@@ -1,11 +1,30 @@
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 
-function CarList(){
-    const cars = useSelector((state) => {
-        return state.cars.data;
-    });
-    console.log(cars);
+function CarList() {
+  const cars = useSelector((state) => {
+    return state.cars.data;
+  });
 
-    return <div>CarList</div>
+  const handleCarDelete = (car) => {};
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button className="button is-danger" onClick={() => handleCarDelete}>
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 export default CarList;
