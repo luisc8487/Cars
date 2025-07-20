74aa1680dd78e91c351be493383241862885da99
L C <LF9904@gmail.com>
2025-07-20 19:41:10 -0400

    update the use of useSelector and createSelector...new update in redux toolkits

--- cars-react/src/components/CarList.jsx
+++ cars-react/src/components/CarList.jsx
@@ -1,14 +1,28 @@
 import {useSelector, useDispatch} from "react-redux";
+import {createSelector} from "@reduxjs/toolkit";
 import {removeCar} from "../store";
+
+const memoizedCars = createSelector(
+  // Updated version with the use of createSelector
+  [(state) => state.cars.data, (state) => state.cars.searchTerm],
+  (data, searchTerm) => {
+    return data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+);
 
 function CarList() {
   const dispatch = useDispatch();
 
-  const cars = useSelector(({cars: {data, searchTerm}}) => {
-    return data.filter((car) =>
-      car.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-  });
+  // Change the useSelector usage since an update in react-redux
+  // const cars = useSelector(({cars: {data, searchTerm}}) => {
+  //   return data.filter((car) =>
+  //     car.name.toLowerCase().includes(searchTerm.toLowerCase())
+  //   );
+  // });
+
+  const cars = useSelector(memoizedCars);
 
   const handleCarDelete = (car) => {
     dispatch(removeCar(car.id));
